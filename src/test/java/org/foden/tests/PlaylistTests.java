703c220de7ebaedf6a0588caed419fe6956c0279
Foden Duong <duongvinh1706@gmail.com>
2024-12-18 21:21:55 +0700

    Add description, owner, epic, story,...

--- src/test/java/org/foden/tests/PlaylistTests.java
+++ src/test/java/org/foden/tests/PlaylistTests.java
@@ -15,6 +15,7 @@ public class PlaylistTests {
 
 
     @Feature("Play List")
+    @Epic("SA-7 Manage Playlists Of Users")
     @Test(description = "Should be able to create a playlist")
     @Story("SA-6")
     @TmsLinks({@TmsLink("FMS-202"), @TmsLink("FMS-204")})
@@ -27,7 +28,42 @@ public class PlaylistTests {
         assertPlaylistEqual(response.as(Playlist.class), requestPlaylist);
     }
 
-    @Test(description = "Should be able to get a playlist")
+    @Feature("Play List")
+    @Epic("SA-7 Manage Playlists Of Users")
+    @Test(description = "Should not be able to create a playlist without name")
+    @Story("SA-6")
+    @TmsLinks({@TmsLink("FMS-202"), @TmsLink("FMS-204")})
+    @Description("Verify should not be able to create a playlist without name")
+    @Owner("Foden Duong")
+    public void shouldNotBeAbleToCreatePlayListWithoutName(){
+        Playlist requestPlaylist = playlistBuilder("", "Top 5 music songs last week in Europe", false);
+        Response response = PlaylistApi.post(requestPlaylist);
+        assertStatusCode(response.statusCode(), 400);
+        assertError(response.as(Error.class), 400, "Missing required field: name");
+    }
+
+    @Feature("Play List")
+    @Epic("SA-7 Manage Playlists Of Users")
+    @Test(description = "Should not be able to create a playlist with expired token")
+    @Story("SA-6")
+    @TmsLinks({@TmsLink("FMS-202"), @TmsLink("FMS-204")})
+    @Description("Verify should not be able to create a playlist with expired token")
+    @Owner("Foden Duong")
+    public void shouldNotBeAbleToCreatePlayListWithExpiredToken(){
+        Playlist requestPlaylist = playlistBuilder("Foden Playlists", "Top 5 music songs last week in Europe.", false);
+        String expiredToken = "1BQDxcdWArcPgYiSjcLud_FIMjc7kK2_3O94q1HdXKEE4aHQpf1nAKSiL0_hOqavh0eAKjhO9-RVGX0hu_CkHzocpaJLL7uxpsXzyDEEUrcdfpfSai9aBaCLSeewIXdRrIbTw_F_wZVb1L7OxUTvqpuqeSZZSVRCQSR3dCU8_aqKunJK1B0wbSwT_4Gu-SY_EoZFGV00-VVlaZsTN_pi6VZaKfKo6h6D80jfwz_WgPV4mYAu_MmKzWlqmowDmeqwPsLPBfT3TOFtMgs7i-R0h7EQU";
+        Response response = PlaylistApi.post(expiredToken ,requestPlaylist);
+        assertStatusCode(response.statusCode(), 401);
+        assertError(response.as(Error.class), 401, "Invalid access token");
+    }
+
+    @Feature("Play List")
+    @Epic("SA-7 Manage Playlists Of Users")
+    @Test(description = "Should be able to get a playlist via api request")
+    @Story("SA-9")
+    @TmsLinks({@TmsLink("FMS-202"), @TmsLink("FMS-204")})
+    @Description("Verify should be able to get a playlist successfully via api request")
+    @Owner("Foden Duong")
     public void getPlayList(){
         Playlist requestPlaylist = playlistBuilder("Foden Like It", "Top 10 songs that I really like.", true);
         Response response = PlaylistApi.get(DataLoader.getInstance().getGetPlaylistId());
@@ -36,28 +72,17 @@ public class PlaylistTests {
 
     }
 
-    @Test(description = "Should be able to update a playlist")
+    @Feature("Play List")
+    @Epic("SA-7 Manage Playlists Of Users")
+    @Test(description = "Should be able to update a playlist via api request")
+    @Story("SA-11")
+    @TmsLinks({@TmsLink("FMS-202"), @TmsLink("FMS-204")})
+    @Description("Verify should be able to update a playlist successfully via api request")
+    @Owner("Foden Duong")
     public void updatePlayList(){
         Playlist requestPlaylist = playlistBuilder("Top 5 Europe music songs", "Top 5 music songs last week in Europe.", false);
         Response response = PlaylistApi.update(DataLoader.getInstance().getUpdatePlaylistId(), requestPlaylist);
         assertStatusCode(response.statusCode(), 200);
-    }
-
-    @Test(description = "Should not be able to create a playlist without name")
-    public void shouldNotBeAbleToCreatePlayListWithoutName(){
-        Playlist requestPlaylist = playlistBuilder("", "Top 5 music songs last week in Europe", false);
-        Response response = PlaylistApi.post(requestPlaylist);
-        assertStatusCode(response.statusCode(), 400);
-        assertError(response.as(Error.class), 400, "Missing required field: name");
-    }
-
-    @Test(description = "Should not be able to create a playlist with expired token")
-    public void shouldNotBeAbleToCreatePlayListWithExpiredToken(){
-        Playlist requestPlaylist = playlistBuilder("Foden Playlists", "Top 5 music songs last week in Europe.", false);
-        String expiredToken = "1BQDxcdWArcPgYiSjcLud_FIMjc7kK2_3O94q1HdXKEE4aHQpf1nAKSiL0_hOqavh0eAKjhO9-RVGX0hu_CkHzocpaJLL7uxpsXzyDEEUrcdfpfSai9aBaCLSeewIXdRrIbTw_F_wZVb1L7OxUTvqpuqeSZZSVRCQSR3dCU8_aqKunJK1B0wbSwT_4Gu-SY_EoZFGV00-VVlaZsTN_pi6VZaKfKo6h6D80jfwz_WgPV4mYAu_MmKzWlqmowDmeqwPsLPBfT3TOFtMgs7i-R0h7EQU";
-        Response response = PlaylistApi.post(expiredToken ,requestPlaylist);
-        assertStatusCode(response.statusCode(), 401);
-        assertError(response.as(Error.class), 401, "Invalid access token");
     }
 
     public Playlist playlistBuilder(String name, String description, boolean _public){
